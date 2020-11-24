96283e8d8587fa6513c870acbbf4b4f7fb8e5497
Kelvin Kitika <lukkitt@live.co.uk>
2020-11-24 10:56:52 +0300

    fix hanoi max recursion error

--- python/hanoi_tower.py
+++ python/hanoi_tower.py
@@ -3,11 +3,13 @@
 # Solution: Move n-1 recursively
 # It is possible for 1 and n-1
 # Induction - prooving someting Recursion - Implementing something
-
+# Keep switching source and target until we get to n
 moves = 0
 def solve_hanoi(n,source,target,spare):
+    # n : number of rings
     global moves
     moves = moves + 1
+    if n > 0:
         solve_hanoi(n-1, source, spare, target)
         target.append(source.pop())
         solve_hanoi(n-1, spare, target, source)
@@ -17,7 +19,7 @@ def solve_hanoi(n,source,target,spare):
     
     # https://blossoms.mit.edu/videos/lessons/towers_hanoi_experiential_recursive_thinking
 
-A = [5,4,3,2,1]
+A = [6,5,4,3,2,1]
 B = []
 C = []
-print(solve_hanoi(5,A,B,C))
+print(solve_hanoi(6,A,B,C))
