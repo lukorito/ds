37b07f658658b91ff0b46e368343032022c7fa32
Kelvin Kitika <lukkitt@live.co.uk>
2021-02-18 23:06:27 +0300

    update linked lists

--- codinguniversity/array/linked_lists.py
+++ codinguniversity/array/linked_lists.py
@@ -1,3 +1,5 @@
+# Linked list uses pointer in the next value 
+# the head offers a pointer to the next node and so forth.
 class Node:
     def __init__(self, value, next=None):
         self.value = value
