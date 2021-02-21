a70dfbc71ba1137a569df409668129b11e35a3e9
Kelvin Kitika <lukkitt@live.co.uk>
2021-02-21 23:53:59 +0300

    add more linked list methods

--- codinguniversity/array/linked_lists.py
+++ codinguniversity/array/linked_lists.py
@@ -6,11 +6,43 @@ class Node:
         self.next = next
         
 class LinkedList: 
-    
+    def __init__(self):
+        self.size = 0
+        self.head = None
+
+    def size(self):
+        return self.size
+
+    def empty(self):
+        return self.head == None
+
+    def value_at(self, index):
+        if not self.head:
+            return None
+        curr = self.head
+        count = 0
+        while curr and count <= index:
+            if index == count:
+                return curr.value
+            count+=1
+            curr=curr.next
+
+    def push_front(value):
+        new_node = Node(value)
+        
+
+
 
 
 def main():
-    pass
+    list = LinkedList()
+    head = Node(3)
+    node1 = Node(1)
+    node2 = Node(2)
+    list.head = head
+    head.next = node1
+    head.next.next = node2
+    print(list.value_at(6))
 
 if __name__ == '__main__':
     main()
