0093b48d52d49080172e8a8e695b68b26e1cbed2
Kelvin Kitika <lukkitt@live.co.uk>
2021-03-31 13:48:14 +0300

    update hash table methods

--- codinguniversity/python/hash_tables.py
+++ codinguniversity/python/hash_tables.py
@@ -12,20 +12,25 @@ class Item:
 class HashTable:
     def __init__(self, size):
         self.size = size
+        self.count = 0
         self.table = [[] for _ in range(size)]
 
     def _hash_function(self, key):
+        #hashes the table
         return int(key) % self.size
 
     def set(self, key, value):
+        #adds a key to the table
         hash_value = self._hash_function(key)
         for item in self.table[hash_value]:
             if item.key == key:
                 item.value = value
                 return 
         self.table[hash_value].append(Item(key, value))
+        self.count+=1
 
     def get(self, key):
+        #gets a key from the table
         hash_value = self._hash_function(key)
         for item in self.table[hash_value]:
             if item.key == key:
@@ -33,12 +38,26 @@ class HashTable:
         raise KeyError("Key not found")
 
     def remove(self, key):
+        #removes a key from the table
         hash_value = self._hash_function(key)
         for index, item in enumerate(self.table[hash_value]):
             if item.key == key:
                 del self.table[hash_value][index]
                 return item
         raise KeyError("Key not found")
+
+    def empty(self):
+        # checks if the table is empty
+        return self.count == 0
+
+    def exists(self, key):
+        #checks if a key exists
+        hash_value = self._hash_function(key)
+        for item in self.table[hash_value]:
+            if item.key == key:
+                return True
+        return False
+    
 
 hashtable = HashTable(5)
 hashtable.set(3, "Kelvin")
